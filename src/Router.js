eda067f33e932fe63a3354e46beafe842f7a3c2a
Joeunji0119 <95282989+Joeunji0119@users.noreply.github.com>
2022-10-09 21:14:00 +0900

    first commit

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Main from './pages/Main/Main';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/main" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
