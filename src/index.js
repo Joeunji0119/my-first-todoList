7a38f5c0a0d88bfd77a9e086e88dda6543437acd
Eunji Jo <eunjijo@Eunjiui-MacBookPro.local>
2022-10-24 15:04:21 +0900

    Feat : 리팩토링 완료

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
+import variables from './styles/variables';
 import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
+  <ThemeProvider theme={{ style: theme, variables }}>
     <GlobalStyle />
     <Router />
   </ThemeProvider>
